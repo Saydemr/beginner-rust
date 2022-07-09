c292477473e4e97b15ce9109c730e0910f823b3c
Abdullah Saydemir <abdullah.saydemir@ozu.edu.tr>
2022-07-09 13:13:00 +0300

    daily

--- src/bin/a19.rs
+++ src/bin/a19.rs
@@ -13,4 +13,23 @@
 // Notes:
 // * Use a HashMap for the furniture store stock
 
-fn main() {}
+use std::collections::HashMap;
+
+fn main() 
+{
+    let mut stocks = HashMap::new();
+    stocks.insert("Chairs", 5);
+    stocks.insert("Beds", 3);
+    stocks.insert("Tables", 2);
+    stocks.insert("Couches", 0);
+
+    let mut total = 0;
+    for (item_name, count) in stocks.iter() {
+        total += count;
+        match count {
+            0 => println!("{:?} is out of stock.", item_name),
+            _ => println!("There are {:?} {:?} in stock.", count, item_name)
+        }
+    }
+    println!("Total number of items: {:?}", total);
+}
