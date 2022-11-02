537c80f06d6faa588d0d770eace8087b8542b95c
Saydemr <37012364+Saydemr@users.noreply.github.com>
2022-11-02 20:00:16 +0100

    project 1

--- src/bin/p1.rs
+++ src/bin/p1.rs
@@ -17,5 +17,115 @@
 //   throughout your program.
 // * Create your program starting at level 1. Once finished, advance to the
 //   next level.
+use std::collections::HashMap;
 
-fn main() {}
+struct Bill {
+    name: String,
+    amount: f32,
+}
+
+struct Bills {
+    bills: HashMap<String, f32>,
+}
+
+impl Bills {
+    fn new() -> Self {
+        Self { bills: HashMap::new() }
+    }
+
+    fn add(&mut self, bill: Bill) {
+        self.bills.insert(bill.name, bill.amount);
+    }
+
+    fn view(&self) {
+        for (name, amount) in &self.bills {
+            println!("{}: ${}", name, amount);
+        }
+    }
+
+    fn remove(&mut self, name: String) -> bool {
+        self.bills.remove(&name).is_some()
+    }
+}
+
+fn add_bills_menu(bills: &mut Bills) {
+    println!("Enter the name of the bill:");
+    let name = get_input();
+
+    println!("Enter the amount of the bill:");
+    let amount = get_input().parse::<f32>().unwrap();
+
+    bills.add(Bill { name, amount });
+    println!("Bill added!");
+}
+
+fn view_bills_menu(bills: &Bills) {
+    bills.view();
+}
+
+fn remove_bills_menu(bills: &mut Bills) {
+    println!("Enter the name of the bill to remove:");
+    let name = get_input();
+
+    let check = bills.remove(name);
+    if check {
+        println!("Bill removed!");
+    } else {
+        println!("Bill not found!");
+    }
+}
+
+fn edit_bills_menu(bills: &mut Bills) {
+    println!("Enter the name of the bill to edit:");
+    let name = get_input();
+
+    println!("Enter the new amount of the bill:");
+    let amount = get_input().parse::<f32>().unwrap();
+
+    let mut checky = bills.remove(name).is_some();
+    if checky {
+        bills.add(Bill { name, amount });
+        println!("Bill edited!");
+    } else {
+        println!("Bill not found!");
+    }
+}
+
+
+fn get_input() -> String {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    input.trim().to_owned()
+}
+
+fn main_menu() {
+    fn show() {
+        println!("
+        1. Add bill
+        2. View bills
+        3. Remove bill
+        4. Edit bill
+        5. Exit
+        ");
+    }
+
+    let mut bills = Bills::new();
+
+    loop {
+        show();
+        let input = get_input();
+        match input.as_str() {
+            "1" => add_bills_menu(&mut bills),
+            "2" => view_bills_menu(&bills),
+            "3" => remove_bills_menu(&mut bills),
+            "4" => edit_bills_menu(&mut bills),
+            "5" => break,
+            _ => println!("Invalid input"),
+        }
+    }
+}
+
+fn main()
+{
+    main_menu();
+}
