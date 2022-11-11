f56d0e1fb3724189a04948a9b0e7bf3321b0726d
Saydemr <37012364+Saydemr@users.noreply.github.com>
2022-11-11 02:32:04 +0100

    Iterators

--- src/bin/a24.rs
+++ src/bin/a24.rs
@@ -10,4 +10,14 @@
 
 fn main() {
     let data = vec![1, 2, 3, 4, 5];
+
+    let triple_filter: Vec<_> = data
+        .iter()
+        .map(|num| num * 3)
+        .filter(|num| num > &10)
+        .collect();
+    
+    for num in triple_filter {
+        println!("{:?}", num);
+    }
 }
