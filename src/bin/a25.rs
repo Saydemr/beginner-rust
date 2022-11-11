5c2552a5b456605596e82480054f5e99cd0ef795
Saydemr <37012364+Saydemr@users.noreply.github.com>
2022-11-11 22:39:49 +0100

    Traits

--- src/bin/a25.rs
+++ src/bin/a25.rs
@@ -12,4 +12,40 @@
 // * Use a single function to print out the perimeter of the shapes
 //   * The function must utilize impl trait as a function parameter
 
-fn main() {}
+trait Perimeter {
+    fn calculate(&self);
+}
+
+struct Triangle {
+    l1 : f32,
+    l2 : f32,
+    l3 : f32,
+}
+
+impl Perimeter for Triangle {
+    fn calculate(&self) {
+        println!("Perimeter is : {:?}", self.l1 + self.l2 + self.l3);
+    }
+}
+
+struct Square {
+    side : f32,
+}
+
+
+impl Perimeter for Square {
+    fn calculate(&self) {
+        println!("Perimeter is : {:?}", self.side * 4.0);
+    }
+}
+
+fn calculation(shape: impl Perimeter) {
+    shape.calculate();
+}
+
+
+fn main()
+{
+    calculation(Square { side: 5.0 });
+    calculation(Triangle {l1 : 3.0, l2: 4.0, l3: 5.0});
+}
